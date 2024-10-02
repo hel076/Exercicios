50a9490a93cbcee6c7e089ee0b6c22ef5c647349
hel076 <heloisaf00@gmail.com>
2024-10-02 10:16:47 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//Exercício 01
+public class Main {
+    public static Scanner void main(String[] args) {
+        Scanner resposta = new Scanner(System.in);
+        System.out.println("Digite sua idade: ");
+
+        if (18 < resposta) {
+            System.out.println("Você pode votar!");
+
+        } else {
+                System.out.println("Você não pode votar");
+            }
+        }
+    }
